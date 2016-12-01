4ad3e5c1e6de890a7854d05172cdfd0e43e2f442
Spencer Maslen <Spencer Maslen>
2016-12-01 15:12:44 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/CPSC_Assign_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPSC_Assign_4.dir/main.cpp.obj"
+  "CPSC_Assign_4.pdb"
+  "CPSC_Assign_4.exe"
+  "CPSC_Assign_4.exe.manifest"
+  "libCPSC_Assign_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPSC_Assign_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include <limits>
+using namespace std;
+
+#define BYTE 16
+#define NEGATIVE_FLOAT 32768
+#define POSITIVE_FLOAT 32767
+#define CHAR 256
+
+int main(int argc, char** argv) {
+
+/*    int input;
+    unsigned short thing = numeric_limits<unsigned short>::max();
+    cout << sizeof(char) << endl;
+    cout << sizeof(short) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(long) << endl;
+    cout << numeric_limits<char>::max() << endl;
+    cout << numeric_limits<unsigned short>::max() << endl;
+    cout << numeric_limits<int>::max() << endl;
+    cout << thing/256 << endl;
+    cout << thing/(256 * 256) << endl;*/
+
+    ifstream audioFile;
+    int fileLength = 0;
+    unsigned short digit;
+    unsigned char part_of_digit;
+
+    audioFile.open("END.wav");
+
+    while(!audioFile.eof())
+    {
+        fileLength++;
+    }
+    audioFile.close();
+    audioFile.open("END.wav");
+    float newValues[fileLength];
+    int index = 0;
+    while(!audioFile.eof())
+    {
+        part_of_digit = audioFile.get();
+        digit = (unsigned short) part_of_digit * CHAR;
+        if(!audioFile.eof())
+        {
+            part_of_digit = audioFile.get();
+            digit += (unsigned short) part_of_digit;
+        }
+
+        if(index >= fileLength)
+        {
+            if(digit < 0)
+            {
+                newValues[index] = (float) digit/NEGATIVE_FLOAT;
+            }
+            else
+            {
+                newValues[index] = (float) digit/POSITIVE_FLOAT;
+            }
+            index++;
+        }
+
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
